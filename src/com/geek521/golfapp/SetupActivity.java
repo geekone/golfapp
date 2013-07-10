2300b4a4249d1e4b9da83969ec5be4813aa0fba5
ajaxj <ajaxj@qq.com>
2013-07-10 12:05:28 +0800

    添加了两个窗口切换

--- src/com/geek521/golfapp/SetupActivity.java
+++ src/com/geek521/golfapp/SetupActivity.java
@@ -7,37 +7,36 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class SetupActivity extends Activity {
 
+	Button btn_home;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_setup);
 		
-		Button btn_setup = (Button)findViewById(R.id.btn_setup);
-		btn_setup.setOnClickListener(new Button.OnClickListener() {
+		btn_home = (Button)findViewById(R.id.btn1);
+		btn_home.setOnClickListener(new Button.OnClickListener(){
+
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent();
-				//TODO 要带过去的参数
-//				intent.putExtra("name", "value");
-				intent.setClass(MainActivity.this, SetupActivity.class);
+				Intent intent = SetupActivity.this.getIntent();
+				intent.setClass(SetupActivity.this, MainActivity.class);
 				startActivity(intent);
-//				MainActivity.this.finish();			//不关的话可以切换
+				SetupActivity.this.finish();
 			}
 			
 		});
+		
 		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.setup, menu);
 		return true;
 	}
 
-	
-	
-	
 }
